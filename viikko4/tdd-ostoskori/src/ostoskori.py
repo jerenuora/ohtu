2c57e8f42e8d3e9953449ff84af3e77fe0c0683f
Jere Nuora <jere.nuora@gmail.com>
2022-02-09 22:51:04 +0200

    15. tyhjennys toimii

--- viikko4/tdd-ostoskori/src/ostoskori.py
+++ viikko4/tdd-ostoskori/src/ostoskori.py
@@ -38,8 +38,7 @@ class Ostoskori:
                 self.kori.remove(ostos)
 
     def tyhjenna(self):
-        pass
-        # tyhjentää ostoskorin
+        self.kori = []
 
     def ostokset(self):
         return self.kori
